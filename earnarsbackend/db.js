dd3293f85e085b49bdcc3a4c583f8905f06719e0
Chukwubuike Kingsley <86000049+Buike369@users.noreply.github.com>
2024-07-26 02:35:37 +0100

    uploads

--- earnarsbackend/db.js
+++ earnarsbackend/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
  const db = mysql.createPool({
-    connectionLimit:30,
+    connectionLimit:90,
     host: `${process.env.DB_HOST}`,
     user: `${process.env.DB_USER}` ,
     password: `${process.env.DB_PASSWORD}` ,
