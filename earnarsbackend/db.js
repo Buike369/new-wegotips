4a9fdb8cf9c02779e48d76f42f50a5b3c23308b0
Chukwubuike Kingsley <86000049+Buike369@users.noreply.github.com>
2024-08-03 03:42:37 +0100

    uploads

--- earnarsbackend/db.js
+++ earnarsbackend/db.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2')
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-   //  port:3306
+    port:3306
    
   
   
